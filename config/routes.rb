eddb881e46268ff4f19546e91c2c458eb8f3ca9c
joey collier <joey.collier.pro@gmail.com>
2025-02-04 21:58:15 +0100

    Watch-list - Avancée avant H

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :bookmarks do
-    resources :movies do
-      resources :lists
-    end
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
 end
